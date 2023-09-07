626b2427816e02d5b33bc9268f6b661e616c5282
Binh Le <lebinh190998@gmail.com>
2023-09-08 00:52:51 +0700

    set up swagger

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 9000;
+const swaggerDocument = require("./swagger.json");
 
-const swaggerSpec = swaggerJsdoc({
-	swaggerDefinition: {
-		info: {
-			title: "Your API Documentation",
-			version: "1.0.0",
-			description: "Documentation for your RESTful API",
-		},
-		basePath: "/",
-	},
-	apis: ["./routes/*.js"],
-});
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/analysis", require("./routes/analysis"));
 
--- routes/analysis.js
+++ routes/analysis.js
@@ -1,29 +1,31 @@
-// routes/analysis.js
-/**
- * @swagger
- * /analysis:
- *   get:
- *     summary: Get single-cell data
- *     responses:
- *       200:
- *         description: Single-cell data
- */
 const express = require("express");
 const router = express.Router();
 const RScriptRunner = require("../models/RScriptRunner");
 
-router.get("/", (req, res) => {
+router.get("/default", (req, res) => {
 	try {
 		// Usage:
 		const rScriptPath = "goolam.R";
 		const rRunner = new RScriptRunner(rScriptPath);
 
-		const dataToR = {
-			name: "Le Binh",
-			description: "This is a test data object",
-		};
+		rRunner.run().then(() => {
+			// Access the parsed data
+			console.log("Parsed Data:", rRunner.parsedData);
+			res.send(rRunner.parsedData);
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Error executing R code");
+	}
+});
 
-		rRunner.sendData(dataToR);
+router.post("/custom-data", (req, res) => {
+	try {
+		// Usage:
+		const rScriptPath = "customData.R";
+		const rRunner = new RScriptRunner(rScriptPath);
+
+		rRunner.sendData(req.body);
 
 		rRunner.run().then(() => {
 			// Access the parsed data
