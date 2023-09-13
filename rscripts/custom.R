220416df6dc14affac067a4f75eb71bcb41e641d
Binh Le <lebinh190998@gmail.com>
2023-09-13 12:07:45 +0700

    cleanup

--- rscripts/custom.R
+++ rscripts/custom.R
@@ -18,25 +18,25 @@ label <- colnames(your_data)
 data <- log2(data + 1)
 
 # Generate clustering result
-# result <- scDHA(data, seed = 1)
+result <- scDHA(data, seed = 1)
 
-# # The clustering result can be found here
-# cluster <- result$cluster
+# The clustering result can be found here
+cluster <- result$cluster
 
-# # Generate 2D representation
-# result <- scDHA.vis(result, seed = 1)
+# Generate 2D representation
+result <- scDHA.vis(result, seed = 1)
 
-# # Function to serialize and print data with a label
-# print_data_with_label <- function(data, label) {
-#   json_data <- toJSON(data, auto_unbox = TRUE)
-#   cat(paste(label, json_data, sep = ":"), "\n")
-# }
+# Function to serialize and print data with a label
+print_data_with_label <- function(data, label) {
+  json_data <- toJSON(data, auto_unbox = TRUE)
+  cat(paste(label, json_data, sep = ":"), "\n")
+}
 
-# # Print cluster data with a label
-# print_data_with_label(result$cluster, "cluster")
+# Print cluster data with a label
+print_data_with_label(result$cluster, "cluster")
 
-# # Print pred data with a label
-# print_data_with_label(result$pred, "pred")
+# Print pred data with a label
+print_data_with_label(result$pred, "pred")
 
-# # Print latent data with a label
-# print_data_with_label(result$latent, "latent")
+# Print latent data with a label
+print_data_with_label(result$latent, "latent")
